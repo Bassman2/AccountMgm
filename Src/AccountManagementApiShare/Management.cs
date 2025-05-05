b3774a1f55e6eae771790dc12a76591fc97e1f15
Ralf Beckers <ralf.beckers@hotmail.com>
2025-05-05 14:58:52 +0200

    Update

--- Src/AccountManagementApiShare/Management.cs
+++ Src/AccountManagementApiShare/Management.cs
@@ -1,5 +1,41 @@
-﻿namespace AccountManagementApiShare;
+﻿using System.Net.NetworkInformation;
+
+namespace AccountManagementApiShare;
 
 internal static class Management
 {
+    internal static readonly string domainName; 
+    internal static PrincipalContext context; 
+
+    static Management()
+    {
+        domainName = IPGlobalProperties.GetIPGlobalProperties().DomainName; 
+        context = new PrincipalContext(ContextType.Domain, domainName);
+    }
+
+    public static IEnumerable<User> GetUsers()
+    {
+        using UserPrincipal u = new UserPrincipal(context);
+        using PrincipalSearcher search = new PrincipalSearcher(u);
+        using var results = search.FindAll();
+        foreach (Principal result in results)
+        {
+            if (result is UserPrincipal user) 
+            { 
+                yield return new User()
+                {
+                    Name = user.Name,
+                    SamAccountName = user.SamAccountName,
+                    UserPrincipalName = user.UserPrincipalName,
+                    EmailAddress = user.EmailAddress,
+                    DisplayName = user.DisplayName,
+                    Description = user.Description,
+                    LastLogon = user.LastLogon,
+                    LastPasswordSet = user.LastPasswordSet,
+                    IsAccountDisabled = user.IsAccountDisabled,
+                };
+            }
+        }
+
+    }
 }
--- Src/AccountManagementApiShare/Usings.cs
+++ Src/AccountManagementApiShare/Usings.cs
@@ -2,4 +2,5 @@
 global using System.Collections.Generic;
 global using System.Text;
 
-
+global using System.Net.NetworkInformation;
+global using System.DirectoryServices.AccountManagement;
